de3a947a215fdd151e79859f64336fdc8867e053
wkdtmdgnsdev <wkdtmdgnsdev@gmail.com>
2025-06-16 16:02:17 +0900

    feat : 도메인 다시 백업

---
+++ back/src/main/java/org/kosa/tripTalk/product/Product.java
@@ -0,0 +1,53 @@
+package org.kosa.tripTalk.product;
+
+import java.util.Locale.Category;
+
+import org.kosa.tripTalk.seller.Seller;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	private Integer price;
+
+	@Column(nullable = false)
+	private String address;
+
+}

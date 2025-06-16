bf542b953621c33d4d2169666a80d0fcc43c85a1
LAPTOP-RDK2MMI9\sungh <sunghyun03315555@gmail.com>
2025-06-16 16:47:31 +0900

    comment, travellog, category 엔티티 추가

---
+++ back/src/main/java/org/kosa/tripTalk/travellog/TravelLog.java
@@ -0,0 +1,45 @@
+package org.kosa.tripTalk.travellog;
+
+import java.time.LocalDateTime;
+
+import org.kosa.tripTalk.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "travellog")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class TravelLog {
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private String category;
+
+    @Column
+    private LocalDateTime createdAt;
+
+}
